0e1dd8f1b24afd85148f0c4c62648f1249efb91c
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2023-05-17 09:21:20 +0300

    Add initial code examples

---
+++ JavaScript/1-Chunk/1-initial.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const ID_LENGTH = 4;
+
+const createIdBuffer = (id) => {
+  const buffer = new ArrayBuffer(ID_LENGTH);
+  const view = new DataView(buffer);
+  view.setInt32(0, id);
+  return buffer;
+};
+
+const getStreamId = (buffer) => {
+  const view = new DataView(buffer);
+  return view.getInt32(0);
+};
+
+class Chunk {
+  static encode(id, payload) {
+    const idView = new Uint8Array(createIdBuffer(id));
+    const chunkView = new Uint8Array(ID_LENGTH + payload.length);
+    chunkView.set(idView);
+    chunkView.set(payload, ID_LENGTH);
+    return chunkView;
+  }
+
+  static decode(chunkView) {
+    const id = getStreamId(chunkView.buffer);
+    const payload = chunkView.subarray(ID_LENGTH);
+    return { id, payload };
+  }
+}
+
+// Usage example
+
+const encoder = new TextEncoder();
+const data = encoder.encode('Hello World');
+const packet = Chunk.encode(123, data);
+console.log(packet);
+
+const { id, payload } = Chunk.decode(packet);
+const decoder = new TextDecoder();
+const text = decoder.decode(payload);
+console.log({ id, payload: text });
+
+module.exports = { Chunk };
